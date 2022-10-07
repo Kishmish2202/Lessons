0ab8c1490fff2045e14ee2bb00fae81809417d74
Kishu <101659868+Kishmish2202@users.noreply.github.com>
2022-10-07 20:02:29 +0300

    Add files via upload

--- class.py
+++ class.py
@@ -1,12 +1,27 @@
-names = ['fireway','Dewedor', 'Silverde', 'StupidNames']
-classes = ['amazon', 'necromancer','paladin','druid']
-levels = [2,2,3,4]
+class Fraction:
+    def __init__(self,numer,denom):
+        self.numer = numer 
+        self.denom = denom 
+    
+    def __str__(self):
+        return f'{self.numer}/{self.denom}'
+    def get_numerator(self):
+        return self.numer
+    def get_denominator(self):
+        return self.denom
+    def __add__ (self,other):
+        new_numer = other.denom * self.numer + other.numer * self.denom
+        new_denom = other.denom * self.denom
+        return Fraction(new_numer,new_denom)
+    def __sub__ (self,other):
+        new_numer = self.numer * other.denom - other.numer * self.denom
+        new_denom = other.denom * self.denom
+        return Fraction(new_numer,new_denom)
 
-def get_stats(name, name_list, class_list, level_list):
-    i = name_list.index(name)
-    char_class = class_list[i]
-    char_level = level_list[i]
-    return char_class, char_level
+    def convert(self):
+        return self.numer/self.denom
 
-player_3 = get_stats('Silverde', names, classes, levels)
-print(player_3)
+a = Fraction(2,3)
+#b = Fraction(4,2)
+
+print(a.convert())
